8ed37ef15936585406072d31ef6cc73f8c3e1a7d
NIA <nia.fti@mail.ru>
2009-11-13 18:17:46 +0600

    Refactoring: no magic constants! (Application::render)

--- Application.cpp
+++ Application.cpp
@@ -1,16 +1,68 @@
 #include "Application.h"
 #include <time.h>
 
+const unsigned VECTORS_IN_MATRIX = sizeof(D3DXMATRIX)/sizeof(D3DXVECTOR4);
+
 namespace
 {
-    const char      *SHADER_FILE = "shader.vsh";
-    const int        WINDOW_SIZE = 600;
-    const D3DCOLOR   BACKGROUND_COLOR = D3DCOLOR_XRGB( 5, 5, 10 );
-    const bool       INITIAL_WIREFRAME_STATE = false;
+    const char       *SHADER_FILE = "shader.vsh";
+    const int         WINDOW_SIZE = 600;
+    const D3DCOLOR    BACKGROUND_COLOR = D3DCOLOR_XRGB( 5, 5, 10 );
+    const bool        INITIAL_WIREFRAME_STATE = false;
+    const unsigned    D3DXVEC_SIZE = sizeof(D3DXVECTOR4);
 
-    const unsigned   D3DXVEC_SIZE = sizeof(D3DXVECTOR4);
+    //---------------- SHADER CONSTANTS ---------------------------
+    //    c0-c3 is the view matrix
+    const unsigned    SHADER_REG_VIEW_MX = 0;
+    //   c4-c7 is the first bone matrix
+    //   c8-c11 is the second bone matrix
+    const unsigned    SHADER_REG_BONE_MX = 4;
+    //    c12 is directional light vector
+    const unsigned    SHADER_REG_DIRECTIONAL_VECTOR = 12;
+    const D3DXVECTOR3 SHADER_VAL_DIRECTIONAL_VECTOR  (0, 1.0f, 0.8f);
+    //    c13 is directional light color
+    const unsigned    SHADER_REG_DIRECTIONAL_COLOR = 13;
+    const D3DCOLOR    SHADER_VAL_DIRECTIONAL_COLOR = D3DCOLOR_XRGB(204, 178, 25);
+    //    c14 is diffuse coefficient
+    const unsigned    SHADER_REG_DIFFUSE_COEF = 14;
+    const float       SHADER_VAL_DIFFUSE_COEF = 0.7f;
+    //    c15 is ambient color
+    const unsigned    SHADER_REG_AMBIENT_COLOR = 15;
+    const D3DCOLOR    SHADER_VAL_AMBIENT_COLOR = D3DCOLOR_XRGB(13, 33, 13);
+    //    c16 is point light color
+    const unsigned    SHADER_REG_POINT_COLOR = 16;
+    const D3DCOLOR    SHADER_VAL_POINT_COLOR = D3DCOLOR_XRGB(25, 153, 153);
+    //    c17 is point light position
+    const unsigned    SHADER_REG_POINT_POSITION = 17;
+    const D3DXVECTOR3 SHADER_VAL_POINT_POSITION  (0.2f, -0.91f, -1.1f);
+    //    c18 are attenuation constants
+    const unsigned    SHADER_REG_ATTENUATION = 18;
+    const D3DXVECTOR3 SHADER_VAL_ATTENUATION  (1.0f, 0, 0.5f);
+    //     c19 is specular coefficient
+    const unsigned    SHADER_REG_SPECULAR_COEF = 19;
+    const float       SHADER_VAL_SPECULAR_COEF = 0.4f;
+    //     c20 is specular constant 'f'
+    const unsigned    SHADER_REG_SPECULAR_F = 20;
+    const float       SHADER_VAL_SPECULAR_F = 15.0f;
+    //     c21 is eye position
+    const unsigned    SHADER_REG_EYE = 21;
+    //     c22 is spot light position
+    const unsigned    SHADER_REG_SPOT_POSITION = 22;
+    const D3DXVECTOR3 SHADER_VAL_SPOT_POSITION  (1.3f, -0.5f, -1.3f);
+    //     c23 is spot light color
+    const unsigned    SHADER_REG_SPOT_COLOR = 23;
+    const D3DCOLOR    SHADER_VAL_SPOT_COLOR = D3DCOLOR_XRGB(217, 255, 0);
+    //     c24 is spot light direction
+    const unsigned    SHADER_REG_SPOT_VECTOR = 24;
+    const D3DXVECTOR3 SHADER_VAL_SPOT_VECTOR  (2.0f, -0.7f, -1.1f);
+    D3DXVECTOR3 spot_vector(2.0f, -0.7f, -1.1f);
+    //     c25 is cos( spot light inner angle )
+    const unsigned    SHADER_REG_SPOT_INNER_ANGLE = 25;
+    const float       SHADER_VAL_SPOT_INNER_ANGLE = D3DX_PI/16.0f;
+    //     c26 is cos( spot light outer angle )
+    const unsigned    SHADER_REG_SPOT_OUTER_ANGLE = 26;
+    const float       SHADER_VAL_SPOT_OUTER_ANGLE = D3DX_PI/12.0f;
 }
-const unsigned VECTORS_IN_MATRIX = sizeof(D3DXMATRIX)/sizeof(D3DXVECTOR4);
 
 Application::Application() :
     d3d(NULL), device(NULL), vertex_decl(NULL), shader(NULL),
@@ -85,49 +137,32 @@ void Application::render()
     check_render( device->SetVertexShader(shader) );
     // Setting constants
     float time = static_cast<float>( clock() )/static_cast<float>( CLOCKS_PER_SEC );
-    //    c0-c3 is the view matrix
-    set_shader_matrix(0, camera.get_matrix());
-    //    c12 is directional light vector
-    D3DXVECTOR3 directional_vector(0, 1.0f, 0.8f);
-    D3DXVec3Normalize(&directional_vector, &directional_vector);
-    set_shader_vector(12, directional_vector);
-    //    c13 is directional light color
-    set_shader_color(13, D3DCOLOR_XRGB(204, 178, 25));
-    //    c14 is diffuse coefficient
-    set_shader_float(14, 0.6f);
-    //    c15 is ambient color
-    set_shader_color(15, D3DCOLOR_XRGB(13, 33, 13));
-    //    c16 is point light color
-    set_shader_color(16, D3DCOLOR_XRGB(25, 153, 153));
-    //    c17 is point light position
-    set_shader_point(17, D3DXVECTOR3(0.2f, -0.91f, -1.1f));
-    //    c18 are attenuation constants
-    set_shader_vector(18, D3DXVECTOR3(1.0f, 0, 0.5f));
-    //     c19 is specular coefficient
-    set_shader_float(19, 0.3f);
-    //     c20 is specular constant 'f'
-    set_shader_float(20, 15.0f);
-    //     c21 is eye position
-    set_shader_point(21, camera.get_eye());
-    //     c22 is spot light position
-    set_shader_point(22, D3DXVECTOR3(1.3f, -0.5f, -1.3f));
-    //     c23 is spot light color
-    set_shader_color(23, D3DCOLOR_XRGB(217, 255, 0));
-    //     c24 is spot light direction
-    D3DXVECTOR3 spot_vector(2.0f, -0.7f, -1.1f);
-    D3DXVec3Normalize(&spot_vector, &spot_vector);
-    set_shader_vector(24, spot_vector);
-    //     c25 is cos( spot light inner angle )
-    set_shader_float(25, cos(D3DX_PI/16.0f));
-    //     c26 is cos( spot light outer angle )
-    set_shader_float(26, cos(D3DX_PI/12.0f));
+    D3DXVECTOR3 directional_vector;
+    D3DXVec3Normalize(&directional_vector, &SHADER_VAL_DIRECTIONAL_VECTOR);
+    D3DXVECTOR3 spot_vector;
+    D3DXVec3Normalize(&spot_vector, &SHADER_VAL_SPOT_VECTOR);
+
+    set_shader_matrix( SHADER_REG_VIEW_MX,            camera.get_matrix());
+    set_shader_vector( SHADER_REG_DIRECTIONAL_VECTOR, directional_vector);
+    set_shader_color(  SHADER_REG_DIRECTIONAL_COLOR,  SHADER_VAL_DIRECTIONAL_COLOR);
+    set_shader_float(  SHADER_REG_DIFFUSE_COEF,       SHADER_VAL_DIFFUSE_COEF);
+    set_shader_color(  SHADER_REG_AMBIENT_COLOR,      SHADER_VAL_AMBIENT_COLOR);
+    set_shader_color(  SHADER_REG_POINT_COLOR,        SHADER_VAL_POINT_COLOR);
+    set_shader_point(  SHADER_REG_POINT_POSITION,     SHADER_VAL_POINT_POSITION);
+    set_shader_vector( SHADER_REG_ATTENUATION,        SHADER_VAL_ATTENUATION);
+    set_shader_float(  SHADER_REG_SPECULAR_COEF,      SHADER_VAL_SPECULAR_COEF);
+    set_shader_float(  SHADER_REG_SPECULAR_F,         SHADER_VAL_SPECULAR_F);
+    set_shader_point(  SHADER_REG_EYE,                camera.get_eye());
+    set_shader_point(  SHADER_REG_SPOT_POSITION,      SHADER_VAL_SPOT_POSITION);
+    set_shader_color(  SHADER_REG_SPOT_COLOR,         SHADER_VAL_SPOT_COLOR);
+    set_shader_vector( SHADER_REG_SPOT_VECTOR,        spot_vector);
+    set_shader_float(  SHADER_REG_SPOT_INNER_ANGLE,   cos(SHADER_VAL_SPOT_INNER_ANGLE));
+    set_shader_float(  SHADER_REG_SPOT_OUTER_ANGLE,   cos(SHADER_VAL_SPOT_OUTER_ANGLE));
     // Draw
     std::list<Model*>::iterator end = models.end();
     for ( std::list<Model*>::iterator iter = models.begin(); iter != end; ++iter )
     {
-        unsigned offset = VECTORS_IN_MATRIX;
-        //   c4-c7 is the first bone matrix
-        //   c8-c11 is the second bone matrix
+        unsigned offset = SHADER_REG_BONE_MX;
         (*iter)->set_bones(time); // re-initialize bones
         for(unsigned i = 0; i < BONES_COUNT; ++i)
         {
